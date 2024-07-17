c5127bc64d86816c3f1a7c2d30bffa9d125a6bd6
Ravicha-Kit <ravicha.p@mologtech.com>
2024-07-17 11:51:21 +0700

    move files from github: mologtech-com

---
+++ MologWMSOpenApi/Api/Shipped.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MologWMSOpenApi.Internal
+{
+    public class MologWMSOpenApiShipped
+    {
+        private MologWMSOpenApiClient mologWMSOpenApiClient;
+
+        public MologWMSOpenApiShipped(MologWMSOpenApiClient mologWMSOpenApiClient)
+        {
+            this.mologWMSOpenApiClient = mologWMSOpenApiClient;
+        }
+
+        public async Task<object> SelectByJob(Dictionary<string, object> dict)
+        {
+            var contents = await ApiRunner.Get("/shipped/by_job",
+                this.mologWMSOpenApiClient.appKey,
+                this.mologWMSOpenApiClient.appSecret,
+                this.mologWMSOpenApiClient.tokenModel.AccessToken,
+                dict
+            );
+            return JsonConvert.DeserializeObject(contents);
+        }
+    }
+}
